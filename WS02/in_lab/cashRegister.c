b4beedb1edc9d414984dbca1e2a8d5c29131160e
irresoco <irresoco-myseneca@github.ca>
2021-01-28 23:43:04 -0500

    Update cashRegister.c

--- WS02/in_lab/cashRegister.c
+++ WS02/in_lab/cashRegister.c
@@ -1,11 +1,48 @@
 //==============================================
-// Name:           Full name here
-// Student Number: #########
-// Email:          userID@myseneca.ca
-// Section:        XXX
+// Name:           Ivan Ray Resoco
+// Student Number: 162592174
+// Email:          irresoco@myseneca.ca
+// Section:        NAA
 // Workshop:       2 (in-lab)
 //==============================================
 
 #define _CRT_SECURE_NO_WARNINGS
 
 // Start your code below:
+
+#include <stdio.h>	
+#pragma warning (disable : 4996)
+int main()
+{
+	int numOwe = 0;
+	double GST, amount, Loonies;
+	
+
+    printf("Please enter the amount to be paid: ");
+	
+	scanf("%d", &numOwe);
+
+	GST = numOwe * .13 + .005;
+	
+	printf("GST: $%1.2lf \n", GST);
+
+	Loonies = (numOwe / 1);
+	
+	scanf("%d");
+
+	printf("Loonies required: %d, balance owing $0.81\n");
+	printf("Quaters required: 2, balance owing $0.06 \n");
+	printf("Dimes required: 0, balance owiung $0,06 \n");
+
+
+
+
+    
+
+	return 0;
+
+}
+
+
+
+
